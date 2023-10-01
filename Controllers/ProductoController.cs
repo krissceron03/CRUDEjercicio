ddb4007782d97bb54beedd1094f616cfcda937ef
CamilaCabrera <camilacabrera464@gmail.com>
2023-10-01 16:57:29 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InicioProyectoClasesCRUD.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: ProductoController
+        public ActionResult Index()
+        {
+            return View(Util.Utils.listaProductos);
+        }
+
+        // GET: ProductoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductoController/Create
+       
+
+        // GET: ProductoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductoController/Edit/5
+        
+
+        // GET: ProductoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductoController/Delete/5
+       
+    }
+}
---
+++ Util/Utils.cs
@@ -0,0 +1,40 @@
+﻿using InicioProyectoClasesCRUD.Models;
+
+namespace InicioProyectoClasesCRUD.Util
+{
+    public class Utils
+    {
+        public static List<Producto> listaProductos = new List<Producto>()
+        {
+            new Producto()
+            {
+                IdProducto = 1,
+                Nombre= "Prodicto 1",
+                Descripcion= "Descripcion 1",
+                Cantidad = 1,
+            },
+            new Producto()
+            {
+                IdProducto = 2,
+                Nombre= "Prodicto 2",
+                Descripcion= "Descripcion 2",
+                Cantidad = 2,
+            },
+            new Producto()
+            {
+                IdProducto = 3,
+                Nombre= "Prodicto 3",
+                Descripcion= "Descripcion 3",
+                Cantidad = 3,
+            },
+            new Producto()
+            {
+                IdProducto = 4,
+                Nombre= "Prodicto 4",
+                Descripcion= "Descripcion 4",
+                Cantidad = 4,
+            },
+
+        };
+    }
+}
